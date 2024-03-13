186c482c2539113d8daf97e40921c4a4bc1d0dfb
henrikohre <158563825+henrikohre@users.noreply.github.com>
2024-03-13 19:24:29 +0100

    spill endringer

--- hangman/scripts/script.js
+++ hangman/scripts/script.js
@@ -10,7 +10,7 @@ const getRandomWord = () => {
 }
 
 //Keyboard knapper
-for (let i = 97; i <= 122 i++){
+for (let i = 97; i <= 122; i++){
     const button = document.createElement("button");
     button.innerText = (String.fromCharCode(i));
     keyboardDiv.appendChild(button);
